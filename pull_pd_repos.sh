606a0042f4d12860b9142a3f50374d8624908114
Mac Xu <mxu@marinsoftware.com>
2017-07-20 22:47:37 +0800

    simplify the script of pulling sample codes.

--- pull_pd_repos.sh
+++ pull_pd_repos.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+echo ""
+echo "================================"
 echo "pull pencilbox data models repo"
+echo "--------------------------------"
 git clone https://github.com/macxu/pencilbox_models.git
 
-echo "pull pencilbox backend api repo"
+echo ""
+echo "================================"
+echo "pull pencilbox api repo"
+echo "--------------------------------"
 git clone https://github.com/macxu/pencilbox_api.git
 
-echo "pull pencilbox front end repo"
+echo ""
+echo "================================"
+echo "pull pencilbox website repo"
+echo "--------------------------------"
 git clone https://github.com/macxu/pencilbox.git
