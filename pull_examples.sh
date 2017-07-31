feeb71e1550c7134cbc3103240c14cf89c7f127e
Mac Xu <mxu@marinsoftware.com>
2017-07-31 20:20:49 +0800

    rename the script file

---
+++ pull_examples.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo ""
+echo "================================"
+echo "pull pencilbox data models repo"
+echo "--------------------------------"
+git clone https://github.com/macxu/pencilbox_models.git
+
+echo ""
+echo "================================"
+echo "pull pencilbox api repo"
+echo "--------------------------------"
+git clone https://github.com/macxu/pencilbox_api.git
+
+echo ""
+echo "================================"
+echo "pull pencilbox website repo"
+echo "--------------------------------"
+git clone https://github.com/macxu/pencilbox.git
